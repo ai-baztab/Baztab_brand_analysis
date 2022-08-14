6300efe585196c2c5af2ee0388faf24901c2905d
s.yaghoobi_pr <data.ai.baztab@gmail.com>
2022-08-14 11:32:57 +0430

    debugged rate of interest

--- Competitors/Google_trends.py
+++ Competitors/Google_trends.py
@@ -13,7 +13,7 @@ class GoogleSearchBrandSts:
         self.kw_combinations = [other_keys]
 
     def rate_of_interest(self, timeframe='today 5-y', geo=''):
-        self.py_trends.build_payload(self.brand_name, cat=0, timeframe=timeframe, geo=geo, gprop='')
+        self.py_trends.build_payload([self.brand_name], cat=0, timeframe=timeframe, geo=geo, gprop='')
         df = self.py_trends.interest_over_time().reset_index()
         return df
 
@@ -28,6 +28,12 @@ class GoogleSearchBrandSts:
             response = requests.get(f'http://google.com/complete/search?client=chrome&q={s}')
             for result in json.loads(response.text)[1]:
                 print(result)
-
+    def region_of_interest(self):
+        country = self.py_trends.interest_by_region(resolution='COUNTRY', inc_low_vol=True, inc_geo_code=False)
+        city = self.py_trends.interest_by_region(resolution='CITY', inc_low_vol=True, inc_geo_code=False)
+        region = self.py_trends.interest_by_region(resolution='REGION', inc_low_vol=True, inc_geo_code=False)
+        return country, city, region
 sample = GoogleSearchBrandSts("عالیس")
+print(sample.rate_of_interest())
 sample.auto_complete('دوغ سنتی')
+res = sample.region_of_interest()
